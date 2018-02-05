b0a2a7fe541befe60d92fa3260eac9e752d7e6e8
Emily Hernandez <bulle.96@windowslive.com>
2018-02-05 13:24:32 +0200

    separated createNewCategory() into several functions.
    Budget is planned with going from one big budget to creating sub-budgets. To be able to do that, the treeview creation had to be separated into smalled functions, so that user can add new categories while still not defining any sub-categories (item-type). I also added a button, that now creates a new category.

--- product_card/treeview_handler.js
+++ product_card/treeview_handler.js
@@ -4,32 +4,61 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let budgetTree = document.querySelector("#budget-tree");
-    console.log(budgetTree);
 
-    function createCategory() {
+    let newCategoryBtn = document.createElement("button");
+    newCategoryBtn.textContent = "Add new category";
+    newCategoryBtn.addEventListener("click", createNewCategory);
+
+    budgetTree.appendChild(newCategoryBtn);
+
+    function createNewCategory() {
         // creating the container to hold everything withing a category
         let categoryContainer = document.createElement("div");
         categoryContainer.className = "category-container";
 
         let category = document.createElement("div");
         category.className = "category";
+        category.textContent = "CATEGORY"; // DELETE AFTER ADDING INPUT FIELDS!!
 
         let itemTypeContainer = document.createElement("div");
         itemTypeContainer.className = "item-type-container";
 
+        budgetTree.appendChild(categoryContainer);
+        categoryContainer.appendChild(category);
+        categoryContainer.appendChild(itemTypeContainer);
+    }
+
+    // these functions need to get the right containers!!
+    function createNewItemType(itemTypeContainer) {
         let itemType= document.createElement("div");
         itemType.className = "item-type";
+        itemType.textContent = "ITEM TYPE"; // DELETE AFTER ADDING INPUT FIELDS!!
 
         let priceRangeContainer= document.createElement("div");
         priceRangeContainer.className = "price-range-container";
 
+        itemTypeContainer.appendChild(itemType);
+
+        createNewPriceRange(priceRangeContainer, "Expensive");
+        createNewPriceRange(priceRangeContainer, "Middle-priced");
+        createNewPriceRange(priceRangeContainer, "Cheap");
+    }
+
+    function createNewPriceRange(priceRangeContainer, priceRangeString) {
         let priceRange = document.createElement("div");
         priceRange.className = "price-range";
+        priceRange.textContent = priceRangeString;
 
         let itemContainer= document.createElement("div");
         itemContainer.className = "item-container";
 
+        priceRangeContainer.appendChild(priceRange);
+    }
+
+    function createNewItem(priceRange) {
         let item = document.createElement("div");
         item.className = "item";
+        item.textContent = "ITEM"; // DELETE AFTER ADDING INPUT FIELDS!!
+
     }
 });
